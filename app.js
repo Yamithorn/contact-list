79f254763c63903a1cc5caa54e1debca730ad225
Yamithorn <alvarezyansi@gmail.com>
2019-10-01 03:07:39 -0400

    add heroku

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ const mongoose = require("mongoose"); // Database
 const database = require("./config/keys").mongoURI; // Specific username and password used to access our database
 const bodyParser = require("body-parser"); // Parse the json we seend to our front end
 
+const path = require('path');
+
 // Routes
 const contacts = require("./routes/api/contacts");
 
-app.get("/", (req, res) => res.send("Hello World"));
+if (process.env.NODE_ENV === 'production') {
+
+    app.use(express.static('frontend/build'));
+
+    app.get('/', (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+
+    });
+
+}
+
+// app.get("/", (req, res) => res.send("Hello World"));
 
 const port = process.env.PORT || 5000;
 
